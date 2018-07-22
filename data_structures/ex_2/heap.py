1bb5f5cd52b1c0b354c84f4389dfe69833ff053c
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-22 18:09:20 -0400

    -heap test now pass with sean solution

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,46 +1,17 @@
-def swap(a, i, j):
-  a[i], a[j] = a[j], a[i]
-  
-def is_heap(a):
-  n = 0
-  m = 0
-  while True:
-    for i in [0, 1]:
-      m += 1
-      if m >= len(a):
-        return True
-      if a[m] > a[n]:
-        return False
-    n += 1
-    
-def sift_down(a, n, max):
-  while True:
-    biggest = n
-    c1 = 2*n + 1
-    c2 = c1 + 1
-    for c in [c1, c2]:
-      if c < max and a[c] > a[biggest]:
-        biggest = c
-    if biggest == n:
-      return
-    swap(a, n, biggest)
-    n = biggest
-
-def heapify(a):
-  i = len(a) / 2 - 1
-  max = len(a)
-  while i >= 0:
-    sift_down(a, i, max)
-    i -= 1
-
 def heapsort(arr):
-  heapify(a)
-  j = len(arr) - 1
-  while j > 0:
-    swap(arr, 0, j)
-    sift_down(arr, 0, j)
-    j -= 1
-        
+  heap = Heap()
+  sorted = []
+
+  for el in arr:
+    heap.insert(el)
+  
+  while heap.get_size() > 0:
+    sorted.append(heap.delete())
+
+  sorted.reverse()
+  
+  return sorted   
+
 class Heap:
   def __init__(self):
     self.storage = [0]
